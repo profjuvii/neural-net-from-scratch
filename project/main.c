9a8187015f84b11fdb95e8c96b08bddb5bd5ccfa
Denys Bondarchuk <profjuvi@gmail.com>
2024-08-01 03:36:21 +0200

    Refresh project files

--- project/main.c
+++ project/main.c
@@ -5,15 +5,19 @@
 #include "data_loader.h"
 #include "utils.h"
 
-void training_model(int input_size, int num_classes) {
+int main() {
+    srand((unsigned int)time(NULL));
+
+    int input_size = 28 * 28;
+    int num_classes = 10;
+
     int num_layers = 3;
 
     float learning_rate = 0.001;
-    float momentum = 0.9;
     float beta1 = 0.9;
     float beta2 = 0.999;
 
-    NeuralNetwork *nn = create_network(num_layers, ADAM, CROSS_ENTROPY, momentum, beta1, beta2, NONE, 0.0);
+    NeuralNetwork *nn = create_network(num_layers, ADAM, CROSS_ENTROPY, 0.0, beta1, beta2, NONE, 0.0);
 
     init_layer(&nn->layers[0], input_size, 128, RELU, 0.0);
     init_layer(&nn->layers[1], 128, 64, RELU, 0.0);
@@ -24,7 +28,9 @@ void training_model(int input_size, int num_classes) {
 
     DataLoader *data_loader = create_data_loader(batch_size, input_size, dataset_path);
 
-    int num_epochs = 1000;
+    int num_epochs = 100;
+
+    // Training loop
     for (int i = 0; i <= num_epochs; ++i) {
         if (load_data(data_loader, num_classes) == -1) {
             fprintf(stderr, "Error: Failed to load data.\n");
@@ -43,12 +49,12 @@ void training_model(int input_size, int num_classes) {
             double loss = loss_func(nn->loss_func, nn->predicts, targets, num_classes);
             total_loss += loss;
 
-            // if (i % 10 == 0) {
-            //     printf("  Image: %d\n  Loss: %f\n", j + 1, loss);
-            //     print_vector("    Predicts: ", nn->predicts, num_classes, 8, 2);
-            //     print_vector("    Targets:  ", targets, num_classes, 8, 2);
-            //     printf("\n");
-            // }
+            if (i % 10 == 0) {
+                printf("  Image: %d\n  Loss: %f\n", j + 1, loss);
+                print_vector("    Predicts: ", nn->predicts, num_classes, 8, 2);
+                print_vector("    Targets:  ", targets, num_classes, 8, 2);
+                printf("\n");
+            }
 
             free(targets);
         }
@@ -62,28 +68,13 @@ void training_model(int input_size, int num_classes) {
         }
     }
 
-    char *models_path = "/Users/profjuvi/neural-net-from-scratch/models/";
-
-    if (save_network(nn, models_path, "model_v1") == 0) {
-        printf("The model was successfully saved.\n");
-    }
-
-    destroy_data_loader(data_loader);
-    destroy_network(nn);
-}
-
-void testing_model(NeuralNetwork *nn, int input_size, int num_classes) {
-    char *dataset_path = "/Users/profjuvi/Datasets/MNIST/";
-    DataLoader *data_loader = create_data_loader(64, input_size, dataset_path);
-
-    int test_size = 1;
+    load_data(data_loader, num_classes);
     int correct_predicts = 0;
 
-    for (int i = 0; i < test_size; ++i) {
-        load_data(data_loader, num_classes);
-
-        int label = data_loader->labels[i];
-        float *features = data_loader->vectors[i];
+    // Testing loop
+    for (int j = 0; j < batch_size; ++j) {
+        int label = data_loader->labels[j];
+        float *features = data_loader->vectors[j];
 
         forward_pass(nn, features);
 
@@ -93,24 +84,14 @@ void testing_model(NeuralNetwork *nn, int input_size, int num_classes) {
         printf("Number %d: %s\n", label, label == max_index ? "True" : "False");
     }
 
-    printf("Accuracy: %.1f%%\n", (float)correct_predicts / test_size * 100);
-    
+    printf("Accuracy: %.1f%%\n", (float)correct_predicts / batch_size * 100);
+
+    if (save_network(nn, "/Users/profjuvi/neural-net-from-scratch/models/", "model_v1") == 0) {
+        printf("The model was successfully saved.\n");
+    }
+
     destroy_data_loader(data_loader);
-}
-
-int main() {
-    srand((unsigned int)time(NULL));
-
-    int input_size = 28 * 28;
-    int num_classes = 10;
-
-    training_model(input_size, num_classes);
-
-    char *nn_path = "/Users/profjuvi/neural-net-from-scratch/models/model_v1.bin";
-    NeuralNetwork *nn = load_network(nn_path);
-
-    testing_model(nn, input_size, num_classes);
-
     destroy_network(nn);
+
     return 0;
 }
